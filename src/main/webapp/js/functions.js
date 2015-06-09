ea633197b7fad06589f3d32bf3a61c9dbe2f4041
max.oliveira <max.oliveira@one.com.br>
2015-06-09 16:37:05 -0300

    Ajuste - Alteração cadastro de cliente (quiosque)

--- src/main/webapp/js/functions.js
+++ src/main/webapp/js/functions.js
@@ -1,63 +1,17 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $("th .ui-chkbox").css("display", "none");
-    $(".ui-chkbox-box").click(function(){
-        if($("tr[aria-selected=true]").size() === 3){
+    $(".ui-chkbox-box").click(function () {
+        if ($("tr[aria-selected=true]").size() === 3) {
             $("tr[aria-selected=false] td").removeClass("ui-selection-column");
         } else {
             $("tr[aria-selected=false] td").addClass("ui-selection-column");
         }
-   });
+    });
 });
 
-/*zera checkbox ao fechar o modal*/
-$(document).ready(function($) {
-    $('#myModal').on('hidden.bs.modal', function (e) {
-      $('input:checkbox').removeAttr('checked');
-      $('#result').val('R$ 0,00');
-    });
- });
-
-String.prototype.formatMoney = function() {
-    var v = this;
-
-    if(v.indexOf('.') === -1) {
-        v = v.replace(/([\d]+)/, "$1,00");
-    }
-
-    v = v.replace(/([\d]+)\.([\d]{1})$/, "$1,$20");
-    v = v.replace(/([\d]+)\.([\d]{2})$/, "$1,$2");
-    v = v.replace(/([\d]+)([\d]{3}),([\d]{2})$/, "$1.$2,$3");
-
-    return v;
-};
-String.prototype.toFloat = function() {
-    var v = this;
-
-    if (!v) return 0;
-    return parseFloat(v.replace(/[\D]+/g, '' ).replace(/([\d]+)(\d{2})$/, "$1.$2"));
-};
-(function(){
-    "use strict";
-
-    var $chs = document.querySelectorAll('input[name="ch[]"]'),
-        $result = document.getElementById('result'),
-        chsArray = Array.prototype.slice.call($chs);
-
-    chsArray.forEach(function(element, index, array){
-        element.addEventListener("click", function(){
-            var v = this.value,
-                result = 0;
-            v = v.toFloat();
-
-            if (this.checked === true) {
-                result = $result.value.toFloat() + parseFloat(v);
-            } else {
-                result = $result.value.toFloat() - parseFloat(v);
-            }
-
-            $result.value = "R$ " + String(result).formatMoney();
-        });
-    });
-
-
-}());
+$(document).ready(function () {
+    $('.clear-input').val("");
+    $('.cpf').mask("999.999.999-99");
+    $('.telefone').mask("(99) 9999-9999");
+    $('.celular').mask("(99) 99999-9999");
+});
